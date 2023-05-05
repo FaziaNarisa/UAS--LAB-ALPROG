35a9ffda87f1dc2cda1c2e7daff6a0bc3ed9ec7f
FaziaNarisa <131874856+FaziaNarisa@users.noreply.github.com>
2023-05-05 23:19:39 +0700

    Update UAS.c

--- UAS.c
+++ UAS.c
@@ -36,6 +36,18 @@ void init(){
     strcpy(perpus[x].penerbit, "KPG");
     perpus[x].tahun = 2017;
     x++;
+    strcpy(perpus[x].judul, "Melati");
+    strcpy(perpus[x].pengarang, "Chacha");
+    strcpy(perpus[x].penerbit, "Simpel Media");
+    perpus[x].tahun = 2020;
+    perpus[x].status = 1;
+    x++;
+    strcpy(perpus[x].judul, "Pulang Pergi");
+    strcpy(perpus[x].pengarang, "Tere Liye");
+    strcpy(perpus[x].penerbit, "Gramedia");
+    perpus[x].tahun = 2021;
+    perpus[x].status = 1;
+    x++;
 }
 
 void input(){
@@ -56,14 +68,48 @@ void input(){
     fclose(fp);
 }
 
+void cari(){
+    char judul[50];
+    int found = 0;
+    
+    printf("\n===========================================================================================================\n");
+    printf("||                                               Mencari Buku                                            ||\n");
+    printf("===========================================================================================================\n");
+    
+    printf("Masukkan judul buku yang ingin dicari: ");
+    fflush(stdin); 
+    fgets(judul, 50, stdin);
+    judul[strcspn(judul, "\n")] = '\0';
+
+    for(i=0; i<x; i++){
+        if(strcmp(judul, perpus[i].judul)==0){
+            printf("\nBuku dengan judul '%s' ditemukan di perpustakaan.\n", perpus[i].judul);
+            printf("Nama Pengarang: %s\n", perpus[i].pengarang);
+            printf("Penerbit: %s\n", perpus[i].penerbit);
+            printf("Tahun Terbit: %d\n", perpus[i].tahun);
+            if(perpus[i].status == 1){
+                printf("Status: Buku Tersedia\n");
+            } else {
+                printf("Status: Buku Tidak Tersedia\n");
+            }
+            found = 1;
+            break;
+        }
+    }
+    if (found == 0) {
+        printf("\nBuku dengan judul '%s' tidak ditemukan di perpustakaan.\n", judul);
+    }
+}
+
 int main(){
     do{
         printf("===========================================================================================================\n");
         printf("||                                          Program Peminjaman Buku                                      ||\n");
         printf("===========================================================================================================\n");
         printf("1.Menampilkan Data Buku\n");
-        printf("2.Menamba Data Buku\n");
-        printf("3.Keluar\n");
+        printf("2.Menambah Data Buku\n");
+        printf("3.Mencari Buku\n);
+        printf("4.Keluar\n");
         printf("\nPilihan: ");
         scanf("%d", &pil);
         switch(pil){
@@ -73,10 +119,13 @@ int main(){
             case 2 :
                 input();
                 break;
-             case 3:
+            case 3:
+                cari();
+                break;
+            case 4:
                 printf("Keluar");
                 break;
         }
-    }while(pil!=3);
+    }while(pil!=4);
 }
 
